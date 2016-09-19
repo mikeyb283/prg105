6a241ac303f8218f4fdd1556d25c7bc61b16c87d
Michael Balling <mballing@students.mchenry.edu>
2016-09-19 15:07:10 -0600

    Add files via upload

---
+++ Module-4/exercise: 99 bottles of beer.py
@@ -0,0 +1,4 @@
+num_beers = 99
+while num_beers > 0:
+    print num_beers, 'bottles of beer on the wall, ',num_beers,' bottles of beer.\n If one of those bottles should happen to fall,', num_beers-1,' bottles of beer on the wall!'
+    num_beers = num_beers -1
