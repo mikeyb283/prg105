382033bb736e102219e201271a5b57601cc2a0cd
Michael Balling <mballing@students.mchenry.edu>
2016-11-11 19:58:48 -0600

    Add files via upload

---
+++ Module-12/Exercise 17.3.py
@@ -0,0 +1,10 @@
+class Point(object):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '(%d, %d)' % (self.x, self.y)
+
+point = Point(3, 4)
+print point
